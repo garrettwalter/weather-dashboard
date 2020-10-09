81e0759f4c49203bf186d7e04142355ef2fb3383
Garrett Walter <gtwalter150@gmail.com>
2020-10-09 00:30:25 -0400

    created site layout, with search that responds with creation of btn

--- script.js
+++ script.js
@@ -2,10 +2,26 @@ $(document).ready(function(){
 
 console.log("Works");
 
+var userText = $("#userText");
+var searchBtn = $("#searchBtn");
+var savedCities = $("#savedCities");
+var cityBtn = $(".cityBtn");
+var weatherBlock = $("#weatherBlock");
+var cityDisplay = $("#cityDisplay");
+var forecastDisplay = $("#forecastDisplay");
 
+function renderSearchedCities(search) {
+    var city = $("<button>");
+    city.addClass("cityBtn");
+    city.text(search);
+    savedCities.append(city);
+}
 
-
-
+searchBtn.on("click", function(){
+    event.preventDefault();
+    var search = userText.val();
+    renderSearchedCities(search);
+})
 
 
 
